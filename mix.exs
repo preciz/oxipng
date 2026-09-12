d34d9ef899d3238e192d4c525e47744cf29a36b6
preciz <barna.kovacs@tutanota.com>
2026-09-12 20:28:04 +0200

    Reduce minimum Elixir version to ~> 1.18 and update CI matrix

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Oxipng.MixProject do
     [
       app: :oxipng,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),

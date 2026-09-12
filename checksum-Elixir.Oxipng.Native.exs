a3c2e2fac22b2f6a4d6dd3d2e6902d607d07b7f7
preciz <barna.kovacs@tutanota.com>
2026-09-12 23:23:07 +0200

    Record checksums for the verified v0.1.0 release archives

--- checksum-Elixir.Oxipng.Native.exs
+++ checksum-Elixir.Oxipng.Native.exs
@@ -1,1 +1,11 @@
-%{}
+%{
+  "liboxipng_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ed471be153f39728e8592a0f6a562d9754eb0479f9e40538cdb0370efbb9fe86",
+  "liboxipng_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dc1d25f523e0a9dd2eb599abd74e8f93f6d5f4160e9fdd0d2bae9b442a32d35a",
+  "liboxipng_nif-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:01de4650326f01f6f11bf3c16c081f0b681650ea780d3aa78a49459a0c0f3504",
+  "liboxipng_nif-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:7fbfb134cdd821189f2e12d64b3527f9376e94c285ac16ee2d4c11ba86f8cd64",
+  "liboxipng_nif-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:f5986e6d248e35ffb85e38c3b0a6652091bba8bda83a1248d7d2d8acfe757b4a",
+  "liboxipng_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:46cd47d812a5a9eae843c73bdc4075d021d24aa1aa42b052c495d9f7ba605ade",
+  "liboxipng_nif-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:4337cf8a0cd0b7cb492394c572b6fb1c1bb0059aeed9ebe7e47b37b9e43a6af7",
+  "oxipng_nif-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:b422f4a1fe4392658695e3584a4fee50bd32db1e2c8938a9aa998c09aac8fd80",
+  "oxipng_nif-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:56799980409982463c71470285a7df3243787e6aab3f8b0f1fb3e4e0c43a82c2"
+}
